cc2d5132005c792351ed83a88f3f127347061f9f
Lee Stratman <stratmanlc10@gmail.com>
2021-02-22 20:46:22 -0600

    Add songs to data file.  Add get all songs endpoint.

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors);
-app.use(express.json);
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/music", (req, res) => {
+  const music = repoContext.songs.findAllSongs();
+  return res.send(music);
+});
 
 const PORT = process.env.PORT || 3000;
 
