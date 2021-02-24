3d20494033824bc35fbb456025d3b4a9921aa8e2
Lee Stratman <stratmanlc10@gmail.com>
2021-02-23 19:57:22 -0600

    Add update and delete endpoints in index.js.

--- index.js
+++ index.js
@@ -26,6 +26,19 @@ app.post("/api/music", [validateSong], (req, res) => {
   return res.send(song);
 });
 
+app.put("/api/music/:id", [validateSong], (req, res) => {
+  const id = req.params.id;
+  const songPropertiesToUpdate = req.body;
+  const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+  return res.send(updatedSong);
+});
+
+app.delete("/api/music/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedDataSet = repoContext.songs.deleteSong(id);
+  return res.send(updatedDataSet);
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
