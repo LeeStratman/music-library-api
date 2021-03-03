f1b172719eab5e60ac484e1086a0f8eb697b2c9c
Lee Stratman <stratmanlc10@gmail.com>
2021-03-02 19:38:01 -0600

    Update port to avoid client port collision. Add postman requests.

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.delete("/api/music/:id", (req, res) => {
   return res.send(updatedDataSet);
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
